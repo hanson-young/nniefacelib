e0ce3ed9b8179fe01677d678618647e85fa95052
unademo <veraguo11@163.com>
2020-04-02 13:05:34 +0800

    fixed:misunderstanding of test.py

--- PFPLD/test.py
+++ PFPLD/test.py
@@ -12,7 +12,7 @@ from pfld.utils import plot_pose_cube
 
 import cv2
 
-def validate(wlfw_val_dataloader, plfd_backbone):
+def validate(wlfw_val_dataloader, plfd_backbone, args):
     plfd_backbone.eval()
 
     with torch.no_grad():
@@ -48,26 +48,26 @@ def validate(wlfw_val_dataloader, plfd_backbone):
             # interpupil_distance = np.sqrt(np.sum((landmarks[:, 60, :] - landmarks[:, 72, :]) ** 2, axis=1))
             error_norm = np.mean(error_diff / interocular_distance)
 
-            # show result 
-            show_img = np.array(np.transpose(img[0].cpu().numpy(), (1, 2, 0)))
-            show_img = (show_img * 255).astype(np.uint8)
-            np.clip(show_img, 0, 255)
 
-            pre_landmark = landmarks[0] * [112, 112]
 
-            # cv2.imwrite("xxx.jpg", show_img)
-            # img_clone = cv2.imread("xxx.jpg")
-            draw  = show_img.copy()
-            yaw = pose[0][0] * 180 / np.pi
-            pitch = pose[0][1] * 180 / np.pi
-            roll = pose[0][2] * 180 / np.pi
-            for (x, y) in pre_landmark.astype(np.int8):
-                # print("x:{0:}, y:{1:}".format(x, y))
-                cv2.circle(draw, (int(x), int(y)), 1, (0,255,0), 1)
-            draw = plot_pose_cube(draw, yaw, pitch, roll, size=draw.shape[0] // 2)
 
-            # cv2.imshow("xx.jpg", draw)
-            # cv2.waitKey(0)
+            if args.is_show:
+                for i in range(landmarks.shape[0]):
+                    # show result
+                    show_img = np.array(np.transpose(img[i].cpu().numpy(), (1, 2, 0)))
+                    show_img = (show_img * 255).astype(np.uint8)
+                    np.clip(show_img, 0, 255)
+                    draw  = show_img.copy()
+                    yaw = pose[i][0] * 180 / np.pi
+                    pitch = pose[i][1] * 180 / np.pi
+                    roll = pose[i][2] * 180 / np.pi
+                    pre_landmark = landmarks[i] * [112, 112]
+                    for (x, y) in pre_landmark.astype(np.int8):
+                        cv2.circle(draw, (int(x), int(y)), 1, (0,255,0), 1)
+                    draw = plot_pose_cube(draw, yaw, pitch, roll, size=draw.shape[0] // 2)
+
+                    cv2.imshow("xx.jpg", draw)
+                    cv2.waitKey(0)
 
         losses.append(loss.cpu().numpy())
         losses_ION.append(error_norm)
@@ -87,13 +87,15 @@ def main(args):
 
     wlfw_val_dataset = WLFWDatasets(args.test_dataset, transform)
     wlfw_val_dataloader = DataLoader(
-        wlfw_val_dataset, batch_size=8, shuffle=False, num_workers=0)
+        wlfw_val_dataset, batch_size=args.batch_size, shuffle=False, num_workers=0)
 
-    validate(wlfw_val_dataloader, plfd_backbone)
+    validate(wlfw_val_dataloader, plfd_backbone, args)
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Testing')
     parser.add_argument('--model_path', default="./models/pretrained/checkpoint_epoch_final.pth", type=str)
+    parser.add_argument('--is_show', default=True, type=bool)
+    parser.add_argument('--batch_size', default=32, type=int)
     parser.add_argument('--test_dataset', default='/home/unaguo/hanson/data/landmark/WFLW191104/test_data/list.txt', type=str)
 
     args = parser.parse_args()
